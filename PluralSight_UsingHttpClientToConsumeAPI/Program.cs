5528fc1068f127c43e58892490bf209077d6ab71
seangnewman <seangnewman@msn.com>
2021-04-18 15:03:43 -0700

    Add project files.

---
+++ PluralSight_UsingHttpClientToConsumeAPI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PluralSight_UsingHttpClientToConsumeAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
