cc2fb9774c51027690876057a7c993a598444466
seangnewman <seangnewman@msn.com>
2021-05-08 01:35:51 -0700

    Creating custom handlers, including Moq framework to simplyfy testing

---
+++ Movies.Client/TestableClassWithApiAccess.cs
@@ -0,0 +1,55 @@
+﻿using Marvin.StreamExtensions;
+using Movies.Client.Models;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Movies.Client
+{
+    public class TestableClassWithApiAccess
+    {
+        private readonly HttpClient _httpClient;
+
+        public TestableClassWithApiAccess(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<Movie> GetMovie(CancellationToken cancellationToken)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, "api/movies/030a43b0-f9a5-405a-811c-bf342524b2be");
+
+
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            request.Headers.AcceptEncoding.Add(new StringWithQualityHeaderValue("gzip"));
+
+            using (var response = await _httpClient.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, cancellationToken))
+            {
+                switch (response.StatusCode)
+                {
+                    case HttpStatusCode.NotFound:
+                        //Log response
+                        Console.WriteLine("The requested movie cannot be found");
+                        return null;
+
+                    case HttpStatusCode.Unauthorized:
+                        // Trigger the authentication flow
+                        throw new UnauthorizedApiAccessException();
+
+                    default:
+                        response.EnsureSuccessStatusCode();
+                        break;
+                }
+
+                var stream = await response.Content.ReadAsStreamAsync();
+                return stream.ReadAndDeserializeFromJson<Movie>();
+            }
+        }
+
+    }
+}
